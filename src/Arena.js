4fb38fc328d624813c6ca0f8c95cb24da4a9b9b0
Burdon Jordan <burdon.jordan@gmail.com>
2025-06-02 09:35:53 +0200

    Heracles 3 part 1

---
+++ src/Arena.js
@@ -0,0 +1,7 @@
+class Arena {
+    constructor(monsters, hero, size = 10) {
+        this.monsters = monsters;
+        this.hero = hero;
+        this.size = size;
+    }
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,64 +1,75 @@
-const MAX_LIFE = 100
+const MAX_LIFE = 100;
 
 class Fighter {
-  constructor(name, strength, dexterity, image) {
-    this.name = name;
-    this.strength = strength;
-    this.dexterity = dexterity;
-    this.life = MAX_LIFE;
-    this.image = image;
-    this.weapon = null;
-    this.shield = null;
-  }
+    constructor(name, strength, dexterity, image, x, y) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+        this.image = image;
+        this.x = x;
+        this.y = y;
+    }
 
-  /**
-   * Launch a strike
-   * @param Fighter defender
-   */
- fight(defender) {
-    let attackPoints = this.getRandomInt(this.getDamage());
-    let damages = Math.max(attackPoints - defender.getDefense(), 0)
-    defender.life = Math.max(defender.life - damages, 0);
-  }
+    /**
+     * Launch a strike
+     * @param Fighter defender
+     */
+    fight(defender) {
+        let attackPoints = this.getRandomInt(this.getDamage());
+        let damages = Math.max(attackPoints - defender.getDefense(), 0);
+        defender.life = Math.max(defender.life - damages, 0);
+    }
 
+    /**
+     * Calculate the value of the defense
+     * @returns
+     */
+    getDamage() {
+        return this.strength;
+    }
 
-  /**
-   * Calculate the value of the defense
-   * @returns
-   */
-  getDamage() {
-    return this.weapon ?
-      this.strength + this.weapon.damage :
-      this.strength;
-  }
+    /**
+     * Calculate the value of the attack
+     * @returns
+     */
+    getDefense() {
+        return this.shield;
+    }
 
+    /**
+     * Make a random number between 1 and 100
+     * @param Number max
+     * @returns Number
+     */
+    getRandomInt(max) {
+        return 1 + Math.floor(Math.random() * max);
+    }
 
-  /**
-   * Calculate the value of the attack
-   * @returns
-   */
-  getDefense() {
-    return this.shield ?
-      this.dexterity + this.shield.protection :
-      this.dexterity;
-  }
-
-  /**
-   * Make a random number between 1 and 100
-   * @param Number max
-   * @returns Number
-   */
-  getRandomInt(max) {
-    return 1 + Math.floor(Math.random() * max);
-  }
-
-
-  /**
-  * Check if the fighters is still alive
-  * @returns Boolean
-  */
-  isAlive() {
-    return this.life > 0
-  }
+    /**
+     * Check if the fighters is still alive
+     * @returns Boolean
+     */
+    isAlive() {
+        return this.life > 0;
+    }
 }
 
+class Hero extends Fighter {
+    constructor() {
+        this.weapon = null;
+        this.shield = null;
+    }
+
+    getDamage() {
+        return this.weapon ? this.strength + this.weapon.damage : this.strength;
+    }
+
+    getDefense() {
+        return this.shield
+            ? this.dexterity + this.shield.protection
+            : this.dexterity;
+    }
+}
+
+class Monster extends Fighter {}
